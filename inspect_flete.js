8174d6401f236bc4fad73d60d7a1a8a01c4f2ab1
azdmichael93-cloud <azdmichael93@gmail.com>
2026-04-12 17:04:12 -0400

    chore(repo): remove report artifacts and ad-hoc data files

--- inspect_flete.js
+++
@@ -1,6 +0,0 @@
-const XLSX = require('xlsx');
-
-const workbook = XLSX.readFile('Reporte de movimientos de dinero Effi 2026-04-06 15_47_45.xls');
-const sheet = workbook.Sheets[workbook.SheetNames[0]];
-const data = XLSX.utils.sheet_to_json(sheet);
-console.log(data.filter(r => String(r['Tipo de movimiento']).includes('Flete')).slice(0, 5));
